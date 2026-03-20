38f6d52eb3cfd1a6274bf3577f65c1bbe1df3545
JakubSosnowsk737 <jakub.sosnowski@student.pk.edu.pl>
2026-03-20 17:51:00 +0100

    Pierwsza strona index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+// Pierwszy front-controller - na razie tylko statyczny powitalnik
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>WypozyczalniaPRO</title>
+</head>
+<body>
+    <h1>WypozyczalniaPRO</h1>
+    <p>Aplikacja w budowie.</p>
+</body>
+</html>
