7d9c98321c4b11bc6796ea32b1353636bfd9f167
JakubSosnowsk737 <jakub.sosnowski@student.pk.edu.pl>
2026-03-22 11:30:00 +0100

    Szkic koncepcyjny schematu bazy

---
+++ database/schema.sql
@@ -0,0 +1,14 @@
+-- Schemat bazy danych - szkic koncepcyjny
+-- WypozyczalniaPRO
+
+-- Zalozenia:
+-- 1) uzytkownicy z rolami: admin / pracownik / klient
+-- 2) sprzet pogrupowany w kategorie
+-- 3) wypozyczenia laczace uzytkownikow ze sprzetem (M:N)
+
+-- TODO:
+-- - tabela users
+-- - tabela categories
+-- - tabela equipment
+-- - tabela rentals + rental_items
+-- - widoki, trigger, funkcja
