ef32160c5821e673a0361913ca00378d2c3ddaed
JakubSosnowsk737 <jakub.sosnowski@student.pk.edu.pl>
2026-05-31 20:35:00 +0200

    Seed danych + skrypt install.sql + autoladowanie schematu w Postgresie

---
+++ database/install.sql
@@ -0,0 +1,8 @@
+-- Kompletna instalacja bazy WypozyczalniaPRO.
+-- Uruchom: psql -U app -d wypozyczalnia -f install.sql
+
+\i schema.sql
+\i views.sql
+\i triggers.sql
+\i functions.sql
+\i seed.sql
---
+++ database/seed.sql
@@ -0,0 +1,41 @@
+-- Dane przykladowe - WypozyczalniaPRO
+-- Hasla bcrypt do testow:
+--   admin@wpro.pl     / admin123
+--   pracownik@wpro.pl / pracownik123
+--   klient@wpro.pl    / klient123
+
+INSERT INTO users (email, password_hash, first_name, last_name, role) VALUES
+('admin@wpro.pl',     '$2y$10$wW5pV2c8e9q0vKQ4Yp9c5e7nL.7tBQz3OrrFcgWf7uZ0pYj7oQ3jq', 'Anna',  'Adminska',  'admin'),
+('pracownik@wpro.pl', '$2y$10$wW5pV2c8e9q0vKQ4Yp9c5e7nL.7tBQz3OrrFcgWf7uZ0pYj7oQ3jq', 'Piotr', 'Pracowski', 'pracownik'),
+('klient@wpro.pl',    '$2y$10$wW5pV2c8e9q0vKQ4Yp9c5e7nL.7tBQz3OrrFcgWf7uZ0pYj7oQ3jq', 'Jan',   'Klientowicz','klient'),
+('klient2@wpro.pl',   '$2y$10$wW5pV2c8e9q0vKQ4Yp9c5e7nL.7tBQz3OrrFcgWf7uZ0pYj7oQ3jq', 'Ewa',   'Wypozyczak','klient');
+
+INSERT INTO user_profiles (user_id, phone, address) VALUES
+(1, '+48 600 000 001', 'ul. Glowna 1, Krakow'),
+(2, '+48 600 000 002', 'ul. Robocza 5, Krakow'),
+(3, '+48 600 000 003', 'ul. Klienta 10, Krakow');
+
+INSERT INTO categories (name, description) VALUES
+('Narzedzia',     'Wiertarki, szlifierki, mloty udarowe'),
+('Ogrod',         'Kosiarki, podkaszarki, pily lancuchowe'),
+('Sport',         'Rowery, sprzet narciarski, namioty'),
+('Elektronika',   'Projektory, drony, kamery sportowe');
+
+INSERT INTO equipment (category_id, name, description, daily_rate, total_quantity, available_quantity) VALUES
+(1, 'Wiertarka udarowa Bosch', 'Moc 800W, walizka, zestaw wiertel', 25.00, 3, 3),
+(1, 'Mlot udarowy SDS',        'Do kucia i wiercenia w betonie',     45.00, 2, 2),
+(2, 'Kosiarka spalinowa',      'Szerokosc koszenia 46cm',            60.00, 4, 4),
+(2, 'Podkaszarka elektryczna', 'Lekka, do trawnikow',                20.00, 5, 5),
+(3, 'Rower MTB 27.5',          'Amortyzator, 21 biegow',             35.00, 6, 6),
+(3, 'Namiot 4-osobowy',        'Wodoodporny, latwy montaz',          30.00, 3, 3),
+(4, 'Projektor FullHD',        'Jasnosc 3000 lumenow, HDMI',         80.00, 2, 2),
+(4, 'Dron z kamera 4K',        'Czas lotu do 30 min, zasieg 5km',   120.00, 1, 1);
+
+-- Przykladowe wypozyczenie (status zakonczony)
+INSERT INTO rentals (user_id, status, start_date, end_date, total_cost) VALUES
+(3, 'zakonczone', '2026-05-01', '2026-05-03', 75.00);
+INSERT INTO rental_items (rental_id, equipment_id, quantity, daily_rate) VALUES
+(1, 1, 1, 25.00);
+-- Powyzszy INSERT zostanie obsluzony przez trigger - zmniejszy available;
+-- nastepnie zwrot przywraca dostepnosc.
+UPDATE equipment SET available_quantity = total_quantity WHERE id = 1;
